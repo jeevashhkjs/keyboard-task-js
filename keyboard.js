4024b876239d76e65e96c8bb0e339654d4d1bf80
Jeeva <jeevasdckap@gmail.com>
2023-03-20 23:03:49 +0530

    fully finished

--- keyboard.js
+++ keyboard.js
@@ -3,7 +3,8 @@ let output = document.querySelector("#userInputs")
 
 //all Buttons
 let buttons = document.querySelectorAll("button")
-console.log(buttons.length)
+
+let tamilLetters = document.querySelectorAll(".tamil")
 
 let cpl = document.querySelector(".cpl")
 let shiftKeysBtn = document.querySelectorAll(".shift")
@@ -17,19 +18,60 @@ let emojsTag = document.querySelector(".emojs")
 
 let atSymbol = document.querySelector(".at")
 
+let editLetters = document.querySelectorAll(".edit")
+
+
+let shiftKeys = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", '" "', "<", ">", "?"]
+let DefaultKeys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/']
+
+let tamilEditkey = ['அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ', 'எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ', 'ஃ']
+
+let notGo = ['ா', 'ி', 'ீ', 'ு', 'ூ', 'ெ', 'ே', 'ை', 'ொ', 'ோ', 'ௌ', '்']
+
+let tamilKeys = ['அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ', 'எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ', 'ஃ', 'க', 'ங', 'ச', 'ஞ', 'ட', 'ண', 'த', 'ந', 'ப', 'ம', 'ய', 'ர', 'ற', 'ல', 'ள', 'ழ', 'வ', 'ன', 'ஜ', 'ஷ', 'ஸ', 'ஹ', 'க்ஷ', 'ா', 'ி', 'ீ', 'ு', 'ூ', 'ெ', 'ே', 'ை', 'ொ', 'ோ', 'ௌ', '்']
+
+let englishLetters = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'com']
+
+let mainDiv = document.querySelector(".go")
+let selectedLang = document.querySelector("#language")
+let welcome = document.querySelector(".welcome")
+welcome.classList.add("showDiv")
+let keyboard = document.querySelector(".keyboard")
+mainDiv.addEventListener("click", () => {
+
+    if (selectedLang.value != "") {
+        if (selectedLang.value == "tamil") {
+            for (let j = 0; j < tamilLetters.length; j++) {
+                tamilLetters[j].innerText = tamilKeys[j]
+            }
+        } else {
+            for (let j = 0; j < tamilLetters.length; j++) {
+                tamilLetters[j].innerText = englishLetters[j]
+            }
+        }
+        welcome.classList.remove("showDiv")
+        keyboard.classList.add("showDiv")
+    }
+    else {
+        alert("Please select language")
+    }
+})
+
+
+
 emojParent.style.display = "none"
-atSymbol.addEventListener("mouseover",()=>{
+atSymbol.addEventListener("mouseover", () => {
     emojParent.style.display = "block"
 
-    emojParent.addEventListener("mouseover",()=>{
+    emojParent.addEventListener("mouseover", () => {
         emojParent.style.display = "block"
     })
-    emojParent.addEventListener("mouseout",()=>{
+    emojParent.addEventListener("mouseout", () => {
         emojParent.style.display = "none"
     })
 
 })
-atSymbol.addEventListener("mouseout",()=>{
+atSymbol.addEventListener("mouseout", () => {
     emojParent.style.display = "none"
 })
 
@@ -43,57 +85,42 @@ let emojs = [
     '&#128530', '&#128531', '&#128532',
     '&#128533', '&#128534', '&#128535',
     '&#128536', '&#128537', '&#128538',
-    '&#128539', '&#128540','&#128542'
-  ]
+    '&#128539', '&#128540', '&#128542'
+]
 
-  for(let k=0;k<emojs.length;k++){
+for (let k = 0; k < emojs.length; k++) {
     let createPara = document.createElement("p")
-    createPara.setAttribute("id","emoj")
+    createPara.setAttribute("id", "emoj")
     createPara.innerHTML = emojs[k]
     emojsTag.append(createPara)
-    createPara.addEventListener("click",(e)=>{
-        output.value +=  e.target.innerText
+    createPara.addEventListener("click", (e) => {
+        output.value += e.target.innerText
     })
-  }
+}
 
-    sun.classList.add("showIc")
-sun.addEventListener("click",()=>{
-    if(sun.classList.contains("fa-sun")){
+sun.classList.add("showIc")
+sun.addEventListener("click", () => {
+    if (sun.classList.contains("fa-sun")) {
         sun.classList.remove("fa-sun")
         sun.classList.add("fa-moon")
     }
-    else{
+    else {
         sun.classList.remove("fa-moon")
         sun.classList.add("fa-sun")
     }
     darkMode()
 })
 
-function darkMode(){
-    if(sun.classList.contains("fa-sun")){
+function darkMode() {
+    if (sun.classList.contains("fa-sun")) {
         document.querySelector(":root").classList.add("darkmode")
     }
-    else{
+    else {
         document.querySelector(":root").classList.remove("darkmode")
     }
 }
 
-
-let shiftKeys = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", '" "', "<", ">", "?"]
-let DefaultKeys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/']
-
-let tamilKeys = [ 'அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ', 'எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ', 'ஃ', 'க', 'ங', 'ச', 'ஞ', 'ட', 'ண', 'த', 'ந', 'ப', 'ம', 'ய', 'ர', 'ற', 'ல', 'ள', 'ழ', 'வ', 'ன', 'ஜ', 'ஶ', 'ஷ', 'ஸ', 'ஹ', 'க்ஷ', 'ா', 'ி', 'ீ', 'ு', 'ூ', 'ெ', 'ே', 'ை', 'ொ', 'ோ', 'ௌ', '்']
-
-
-
-
-
-
-
-
-
-
-
+// console.log(tamilKeys.length,"arra")
 
 function shiftChangeKey() {
     for (let i = 0; i < buttons.length; i++) {
@@ -114,7 +141,7 @@ for (let i = 0; i < buttons.length; i++) {
         }
         else if (buttons[i].classList.contains("cpl")) {
             capsLocks(cpl)
-            
+
         }
         else if (buttons[i].classList.contains("enter")) {
             output.value += "\n"
@@ -127,8 +154,8 @@ for (let i = 0; i < buttons.length; i++) {
         }
         else if (buttons[i].classList.contains("shift")) {
             let element = e.target
-            ShiftcapsLock(element)           
-        }else if(buttons[i].innerText == "Emojs"){
+            ShiftcapsLock(element)
+        } else if (buttons[i].innerText == "Emojs") {
             output.value += ""
         }
         else {
@@ -160,12 +187,12 @@ window.addEventListener("keydown", (e) => {
         capsLocks(cpl)
     }
     else if (e.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT && e.keyCode == 16) {
-        ShiftcapsLock(shiftKey1) 
+        ShiftcapsLock(shiftKey1)
     }
     else if (e.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT && e.keyCode == 16) {
-        ShiftcapsLock(shiftKey2) 
+        ShiftcapsLock(shiftKey2)
     }
-    else if(e.keyCode == 32){
+    else if (e.keyCode == 32) {
         output.value += " "
         space.classList.add("showClicked")
         setTime(space)
@@ -174,17 +201,6 @@ window.addEventListener("keydown", (e) => {
         output.value += e.key
     }
 })
-
-function changeKys(getKeyElement) {
-    for (let i = 0; i < changeKeys.length; i++) {
-        if (getKeyElement.classList.contains("actives")) {
-            changeKeys[i].innerText = shiftKeys[i]
-        }
-        else{
-            changeKeys[i].innerText = DefaultKeys[i]
-        }
-    }
-}
 
 function setTime(targeter) {
     setTimeout(() => {
@@ -199,35 +215,41 @@ function deleteBtn() {
 
 
 
-function ShiftcapsLock(eleMent){
+function ShiftcapsLock(eleMent) {
     eleMent.classList.toggle("actives")
     eleMent.classList.add("showClicked")
     setTime(eleMent)
-    if(shiftKey1.classList.contains("actives") && shiftKey2.classList.contains("actives")){
+    if (shiftKey1.classList.contains("actives") && shiftKey2.classList.contains("actives")) {
         shiftKey1.classList.remove("actives")
         shiftKey2.classList.remove("actives")
     }
 
-    if((shiftKey1.classList.contains("actives") || shiftKey2.classList.contains("actives"))){
+    if ((shiftKey1.classList.contains("actives") || shiftKey2.classList.contains("actives"))) {
 
-        for(let k=0;k<buttons.length;k++){
+        for (let k = 0; k < buttons.length; k++) {
             if (buttons[k].innerText != "CapsLock" && buttons[k].innerText != "Backspace" && buttons[k].innerText != "Tab" && buttons[k].innerText != "Enter" && buttons[k].innerText != "Shift" && buttons[k].innerText != "Space" && buttons[k].innerText != "com" && buttons[k].innerText != "Emojs") {
                 buttons[k].innerText = buttons[k].innerText.toUpperCase()
             }
         }
-        for(let i=0;i<changeKeys.length;i++){
-            changeKeys[i].innerText = shiftKeys[i]
+        if (selectedLang.value == "tamil") {
+            for (let i = 0; i < tamilEditkey.length; i++) {
+                editLetters[i].innerText = tamilEditkey[i]
+            }
+        } else if (selectedLang.value == "english") {
+            for (let i = 0; i < changeKeys.length; i++) {
+                changeKeys[i].innerText = shiftKeys[i]
+            }
         }
 
     }
-    else if(!(shiftKey1.classList.contains("actives") || shiftKey2.classList.contains("actives"))){
-        
-        for(let k=0;k<buttons.length;k++){
-            if (buttons[k].innerText != "CapsLock" && buttons[k].innerText != "Backspace" && buttons[k].innerText != "Tab" && buttons[k].innerText !=    "Enter" && buttons[k].innerText != "Shift" && buttons[k].innerText != "Space" && buttons[k].innerText != "com" && buttons[k].innerText != "Emojs") {
+    else if (!(shiftKey1.classList.contains("actives") || shiftKey2.classList.contains("actives"))) {
+
+        for (let k = 0; k < buttons.length; k++) {
+            if (buttons[k].innerText != "CapsLock" && buttons[k].innerText != "Backspace" && buttons[k].innerText != "Tab" && buttons[k].innerText != "Enter" && buttons[k].innerText != "Shift" && buttons[k].innerText != "Space" && buttons[k].innerText != "com" && buttons[k].innerText != "Emojs") {
                 buttons[k].innerText = buttons[k].innerText.toLowerCase()
             }
         }
-        for(let i=0;i<changeKeys.length;i++){
+        for (let i = 0; i < changeKeys.length; i++) {
             changeKeys[i].innerText = DefaultKeys[i]
         }
 
@@ -250,9 +272,8 @@ function capsLocks(diff) {
 }
 
 function disable() {
-    document.onkeydown=function(e)
-    {
-    return false;
-    } 
+    document.onkeydown = function (e) {
+        return false;
+    }
 }
 
